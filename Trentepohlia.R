18e5fd23e11d8c8751b045c781e0b860dea2e2bf
Bryan <resendebryan@gmail.com>
2022-01-10 21:31:51 -0300

    Inicial

---
+++ Trentepohlia.R
@@ -0,0 +1,16 @@
+# projeto para webscraping de dados
+# Algas do gênero Trentepohlia podem ser encontradas em vida livre e como parte 
+# fotobionte de de liquens. Uma dúvida é sobre a diferença filogenética desses 
+# dois grupos. Essas algas possuem duas regiões de barcode utilizadas
+
+# O ITS e rcbL.
+# Será que essas regiões realizam os mesmos agrupamentos (clusters)?
+
+
+# Será que dá para fazer um dashboard com esses dados?
+# Para colunas podem ser
+  # Origem
+  # Região barcode
+  # Substrato (se é vida livre ou parte fotossintetizante de líquen)
+  # Se possui voucher
+  # Se é uma sequencia presente em publicação
